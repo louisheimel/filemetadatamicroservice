4a425747ffdd0547d0bebbb6aba87ad5ab5fb26b
Unknown Name <louis.heimel@mines.sdsmt.edu>
2017-01-14 21:29:09 +0000

    finish first draft of project

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 var express = require('express');
+var path = require('path');
 var app = express();
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  res.redirect('/static/index.html');
+});
+
+app.post('/filesubmit', upload.single('attachment'),  function(req, res) {
+  res.json(req.file.size);
+});
 
 app.listen(8080);
